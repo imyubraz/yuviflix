bc7df68b2fd73826e30189f708e00d84835037ba
Yubraj <iyubrajpoudel@gmail.com>
2023-05-22 00:29:42 +0545

    Clearing clutter and setting up basic layout...

---
+++ src/app/layout.jsx
@@ -0,0 +1,22 @@
+import './globals.css'
+
+// fonts
+import { Inter } from 'next/font/google'
+const inter = Inter({ subsets: ['latin'] })
+
+export const metadata = {
+  title: 'Yuviflix | Home',
+  description: 'Yuviflix',
+}
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body className={inter.className}>
+        <header></header>
+        <main>{children}</main>
+        <footer></footer>
+      </body>
+    </html>
+  )
+}
